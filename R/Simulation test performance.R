c0e3bad68f3807c4885de434ac3d1c93cefb50d8
saissaeth <saissa@student.ethz.ch>
2024-03-25 16:19:09 +0100

    Major changes

--- R/Simulation test performance.R
+++ R/Simulation test performance.R
@@ -1,16 +1,14 @@
 ## Libraries##################
-require(dplyr)
-require(randomForest)
-require(Hmisc)
-require(glue)
-require(tidyverse)
-require(purrr)
-require(tibble)
+library(randomForest)
+library(Hmisc)
+library(glue)
+library(tidyverse)
+library(grf)
 ##############################
 
 source("R/dgp.R") #source the dgp function
 ############################# Apply DGP to simulate data and test performance of random forest ##############################
-dgp1 <-
+dgp1_zero <-
   dgp(
     p = 0.5,
     m = 0,
@@ -20,103 +18,133 @@ dgp1 <-
     model = c("normal"),
     xmodel = c("normal"),
     rmvar = NULL
-  ) #continuous outcome
-#dgp2 <- dgp(p = 0.5, m = 0, t = 0, sd = 1, ol = 0, model = c("weibull"), xmodel = c("normal"), rmvar = NULL)
-dgp3 <-
+  )
+dgp1_small <-
+  dgp(
+    p = 0.5,
+    m = 0,
+    t = 0.25,
+    sd = 1,
+    ol = 0,
+    model = c("normal"),
+    xmodel = c("normal"),
+    rmvar = NULL
+  )
+dgp1_med <-
+  dgp(
+    p = 0.5,
+    m = 0,
+    t = 0.5,
+    sd = 1,
+    ol = 0,
+    model = c("normal"),
+    xmodel = c("normal"),
+    rmvar = NULL
+  )
+dgp1_big <-
+  dgp(
+    p = 0.5,
+    m = 0,
+    t = 0.75,
+    sd = 1,
+    ol = 0,
+    model = c("normal"),
+    xmodel = c("normal"),
+    rmvar = NULL
+  )
+dgp1_one <-
   dgp(
     p = 0.5,
     m = 0,
     t = 0,
     sd = 1,
     ol = 0,
-    model = c("binomial"),
+    model = c("normal"),
     xmodel = c("normal"),
     rmvar = NULL
-  ) #binary outcome
-# dgp4 <-
-#   dgp(
-#     p = 0.5,
-#     m = 0,
-#     t = 0,
-#     sd = 1,
-#     ol = 0,
-#     model = c("polr"),
-#     xmodel = c("normal"),
-#     rmvar = NULL
-#   ) #ordinal outcome
+  )
+#continuous outcome
 
-dgp5 <-
-  dgp(
-    p = 0.5,
-    m = 0,
-    t = 0,
-    sd = 1,
-    ol = 0,
-    model = c("normal"),
-    xmodel = c("unif"),
-    rmvar = NULL
-  ) # continuous outcome
-#dgp6 <- dgp(p = 0.5, m = 0, t = 0, sd = 1, ol = 0, model = c("weibull"), xmodel = c("unif"), rmvar = NULL)
-dgp7 <-
-  dgp(
-    p = 0.5,
-    m = 0,
-    t = 0,
-    sd = 1,
-    ol = 0,
-    model = c("binomial"),
-    xmodel = c("unif"),
-    rmvar = NULL
-  ) #binary outcome
-# dgp8 <-
+# dgp5 <-
 #   dgp(
 #     p = 0.5,
 #     m = 0,
-#     t = 0,
+#     t = 0.123,
 #     sd = 1,
 #     ol = 0,
-#     model = c("polr"),
+#     model = c("normal"),
 #     xmodel = c("unif"),
-#     rmvar = NULL
-#   ) #ordinal outcome
+#     rmvar = 10
+#   ) # continuous outcome
+
+
 
 ######################### simulate ###############
-num_sim <- 1000
-sim1 <- simulate.dgp(dgp1, num_sim)
+num_sim <- 1600
+sim1 <- simulate.dgp(dgp1, num_sim,dim=20)
+str(sim1)
 #sim2 <- simulate.dgp(dgp2, num_sim)
-sim3 <- simulate.dgp(dgp3, num_sim)
+# sim3 <- simulate.dgp(dgp3, num_sim)
 # sim4 <- simulate.dgp(dgp4, num_sim)
-sim5 <- simulate.dgp(dgp5, num_sim)
+# sim5 <- predict.dgp(dgp5, num_sim,dim = 20)
 #sim6 <- simulate.dgp(dgp6, num_sim)
-sim7 <- simulate.dgp(dgp7, num_sim)
+# sim7 <- simulate.dgp(dgp7, num_sim)
 #sim8 <- simulate.dgp(dgp8, num_sim)
 #################################################
-
-
-# Create a train test split of simulated data ##################
-train_test_split <- function(data, train_size = 0.8) {
-  train_index <- sample(1:nrow(data), train_size * nrow(data))
-  train <- data[train_index, ]
-  test <- data[-train_index, ]
-  return(list(train = train, test = test))
+apply_cf <- function(dgp, num_sim, dim) {
+  sim <- simulate.dgp(dgp, num_sim,dim=20)
+  excluded_vars <- c("y", "trt")
+  x_vars <- paste0("X", 1:dim)
+  X <- as.matrix(sim[, c(x_vars), drop = FALSE])
+  Y <- c(sim$y)
+  W <- as.numeric(c(sim$trt))
+  rf <- causal_forest(X=as.matrix(X), Y=Y, W=W, num.trees=1000, seed=290875)
+  pml <- predict(rf, type = "coef", OOB =TRUE)
+  pm <- do.call("rbind", pml)
+  pmd <- as.data.frame(pm)
+  names(pmd)[length(names(pmd))] <- "tau"
+  return(pmd$tau)
 }
 
-######### Apply train test split to all simulated data ##############
-sim1_split <- train_test_split(sim1)
-#sim2_split <- train_test_split(sim2)
-sim3_split <- train_test_split(sim3)
-#sim4_split <- train_test_split(sim4)
-sim5_split <- train_test_split(sim5)
-#sim6_split <- train_test_split(sim6)
-sim7_split <- train_test_split(sim7)
-# sim8_split <- train_test_split(sim8)
-########################################################################
+pmd_A_1 <- apply_cf(dgp1_zero, 1600, 20)
+pmd_A_2 <- apply_cf(dgp1_small, 1600, 20)
+pmd_A_3 <- apply_cf(dgp1_med, 1600, 20)
+pmd_A_4 <- apply_cf(dgp1_big, 1600, 20)
+pmd_A_5 <- apply_cf(dgp1_one, 1600, 20)
+
+#Add lines to ggplot with tau of model B and C
+ptau <- ggplot(pmd_A, aes(tau)) +
+  geom_density() +
+  xlab(expression(hat(tau)(bold(x)))) +
+  scale_linetype_manual(name = "", values = 2, guide = guide_legend(override.aes = list(color = c("black")))) +
+  theme(legend.position = c(0.2, 0.8), legend.title = element_blank())
+
+ptau <- ggplot(pmd, aes(tau)) +
+  geom_density() +
+  xlab(expression(hat(tau)(bold(x)))) +
+  scale_linetype_manual(name = "", values = 2, guide = guide_legend(override.aes = list(color = c("black")))) +
+  theme(legend.position = c(0.2, 0.8), legend.title = element_blank())
+ptau
+
+
+# Apply the function to the dgp1
+pmd1 <- apply_cf(dgp1, 1600, 20)
+pmd2 <-
+
+
+
+
+
+
+
+
+
 
 
 #Find MSE on test performance
 mse <- function(train, test, model) {
   rf <-
-    randomForest(y ~ ., data = train[, c("y",  unlist(strsplit(model, "\\+")))], ntree = 100)
+    causal_forest(y ~ ., data = train[, c("y",  unlist(strsplit(model, "\\+")))], ntree = 100)
   y_hat <- predict(rf, test)
   return(mean((as.numeric(y_hat) - test$y) ^ 2)) #return MSE
 }
@@ -129,114 +157,3 @@ missclassification_rate <- function(train, test, model) {
   return(sum(y_hat != test$y) / length(test$y)) #return missclassification rate
 }
 
-#Find concordance index
-concordance.index <- function(train, test, model) {
-  rf <- randomForest(y ~ paste0(model), data = train, ntree = 100)
-  y_hat <- predict(rf, test)
-  return(y_hat) #return concordance index
-  #Find err.rate on predictions on test data
-}
-############ Create all combinations of X1, X2, X3, X4 of size 3,2,1
-
-#glue rows into one string x1+x2+x3
-all_combinations <- map(1:3, ~ combn(c("X1", "X2", "X3", "X4"), .x, simplify = FALSE))  |>
-  flatten() |>
-  map(~ glue_collapse(.x, "+")) |>
-  unlist()
-
-# Create a data frame with combinations and their sizes
-combinations_df <- map_dfr(1:3, ~ {
-  size <- .x
-  combinations <-
-    combn(c("X1", "X2", "X3", "X4"), size, simplify = FALSE) |>
-    map( ~ glue_collapse(.x, "+")) |>
-    unlist()
-  data.frame(Model = combinations, size = size)
-})
-
-
-#Create an empty tibble with columns for MSE, Model_Size, and Variables
-result_normal_dgp1 <- tibble(
-  MSE = numeric(length(combinations_df$Model)),
-  Model_Size = combinations_df$size,
-  Variables = combinations_df$Model
-)
-
-# DGP 1
-for (i in 1:length(combinations_df$Model)) {
-  mse_result <- mse(sim1_split$train, sim1_split$test, model = combinations_df$Model[i])
-  result_normal_dgp1$MSE[i] <- mse_result
-}
-#Create an empty tibble with columns for MSE, Model_Size, and Variables
-result_normal_dgp5 <- tibble(
-  MSE = numeric(length(combinations_df$Model)),
-  Model_Size = combinations_df$size,
-  Variables = combinations_df$Model
-)
-# DGP 5
-for (i in 1:length(combinations_df$Model)) {
-  mse_result <- mse(sim5_split$train, sim5_split$test, model = combinations_df$Model[i])
-  result_normal_dgp5$MSE[i] <- mse_result
-}
-
-#Create an empty tibble with columns for missclassification, Model_Size, and Variables
-result_binomial_dgp3 <- tibble(
-  Missclassification = numeric(length(combinations_df$Model)),
-  Model_Size = combinations_df$size,
-  Variables = combinations_df$Model
-)
-
-# DGP 3
-for (i in 1:length(combinations_df$Model)) {
-  miss_result <- missclassification_rate(sim3_split$train, sim3_split$test, model = combinations_df$Model[i])
-  result_binomial_dgp3$Missclassification[i] <- miss_result
-}
-
-# Create an empty tibble with columns for missclassification, Model_Size, and Variables
-result_binomial_dgp7 <- tibble(
-  Missclassification = numeric(length(combinations_df$Model)),
-  Model_Size = combinations_df$size,
-  Variables = combinations_df$Model
-)
-
-# DGP 7
-for (i in 1:length(combinations_df$Model)) {
-  miss_result <- missclassification_rate(sim7_split$train, sim7_split$test, model = combinations_df$Model[i])
-  result_binomial_dgp7$Missclassification[i] <- miss_result
-}
-
-ggplot(result_normal_dgp1, aes(x = Model_Size, y = MSE, color = reorder(Variables, Model_Size))) +
-  geom_point() +
-  geom_line() +
-  labs(title = "MSE Rates for DGP 1",
-       x = "Model Size",
-       y = "MSE") +
-  theme_minimal() +
-  guides(color = guide_legend(title = "Variables"))
-
-ggplot(result_normal_dgp5, aes(x = Model_Size, y = MSE, color = reorder(Variables, Model_Size))) +
-  geom_point() +
-  geom_line() +
-  labs(title = "MSE Rates for DGP 5",
-       x = "Model Size",
-       y = "MSE") +
-  theme_minimal() +
-  guides(color = guide_legend(title = "Variables"))
-
-ggplot(result_binomial_dgp3, aes(x = Model_Size, y = Missclassification, color = reorder(Variables, Model_Size))) +
-  geom_point() +
-  geom_line() +
-  labs(title = "Missclassification Rates for DGP 3",
-       x = "Model Size",
-       y = "Missclassification") +
-  theme_minimal() +
-  guides(color = guide_legend(title = "Variables"))
-
-ggplot(result_binomial_dgp7, aes(x = Model_Size, y = Missclassification, color = reorder(Variables, Model_Size))) +
-  geom_point() +
-  geom_line() +
-  labs(title = "Missclassification Rates for DGP 7",
-       x = "Model Size",
-       y = "Missclassification") +
-  theme_minimal() +
-  guides(color = guide_legend(title = "Variables"))
