eff7d9290300785a7ef23fd2ca1ea61a640792ad
saissaeth <saissa@student.ethz.ch>
2024-04-25 16:29:01 +0200

    repl =1000

--- R/Simulation_test_performance.R
+++ R/Simulation_test_performance.R
@@ -75,7 +75,7 @@ results <-
   expand_grid(
     Setup = LETTERS[1:4],
     Varmiss = paste0(dim_vec),
-    repl = seq(1, 10, 1),
+    repl = seq(1, 1000, 1),
     rho = c(0,0.25,0.5,0.75)
   ) %>%
   mutate(Setup = as.factor(Setup)) %>%
