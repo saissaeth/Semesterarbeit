a5c3bcec2a528dac796ae41b7441d0928f984011
saissaeth <saissa@student.ethz.ch>
2024-04-28 19:22:06 +0200

    final plotting

--- R/Simulation_test_performance.R
+++ R/Simulation_test_performance.R
@@ -182,13 +182,67 @@ cols <- RColorBrewer::brewer.pal(6,"Spectral")
 
 p <- ggplot(results, aes(x = Setup, y = MSE, fill = Varmiss)) +
   geom_violin() +
-  theme_minimal() +
-  geom_vline(xintercept = c(1.5,2.5,3.5), linetype = "solid", color = "black") +
+  paper_theme() +
+  geom_vline(xintercept = c(1.5,2.5,3.5), linetype = "solid", color = "darkslategray") +
   scale_y_continuous(limits = c(0, 0.6)) +
   facet_wrap(~ rho, scales = 'free_x', ncol = 1, labeller = labeller(rho = label_parsed)) +
   theme(legend.position = "right") +
   labs(fill = "Varmiss") +
   scale_fill_manual(values = cols)
 
+paper_theme <- function() {
+  theme(
+    # plot
+    plot.title = element_text(
+      face = "bold",
+      size = rel(.9),
+      hjust = .5,
+      vjust = 2.5,
+      color = "#130f09"
+    ),
+    plot.subtitle = element_text(size = rel(.7), hjust = .5, margin = margin(b = 10, unit = "pt")),
+    plot.caption = element_text(size = rel(.7), hjust = 1),
+    plot.background = element_rect(fill = "#FFFFFF", color = NA),
+    # panel
+    panel.background = element_rect(fill = "#FFFFFF", color = NA),
+    panel.border = element_rect("#a5a5a5", fill = "transparent", linewidth = rel(2)),
+    panel.grid.major = element_line(colour = "#eeeeee", linewidth = rel(1.2)),
+    panel.grid.minor = element_blank(),
+    # axis
+    axis.ticks = element_line(color = "#a5a5a5"),
+    axis.text = element_text(size = rel(.8)),
+    axis.title.x = element_text(vjust = -.2),
+    axis.title.y = element_text(angle = 90, vjust = 2),
+    axis.text.y = element_text(
+      size = rel(.8),
+      vjust = 0.2,
+      hjust = 1,
+      margin = margin(r = 3)
+    ),
+    axis.text.x = element_text(size = rel(.8), margin = margin(2, 0, 0, 0)),
+    axis.title = element_text(face = "bold", size = rel(.8)),
+    # legend
+    legend.position = "bottom",
+    legend.background = element_rect(fill = "transparent", color = NA),
+    legend.title = element_text(
+      face = "italic",
+      size = rel(.8),
+      hjust = .5
+    ),
+    legend.direction = "vertical",
+    legend.text = element_text(size = rel(.8), vjust = 1),
+    legend.box.spacing = unit(.2, "cm"),
+    legend.key = element_rect(fill = "transparent", color = "transparent"),
+    legend.key.size = unit(.3, "cm"),
+    # facets
+    strip.background = element_rect(fill = "#a5a5a5", color = NA),
+    strip.text = element_text(
+      face = "bold",
+      size = rel(.7),
+      margin = margin(t = 2.5, b = 2.5)
+    )
+  )
+}
 
 print(p)
+
